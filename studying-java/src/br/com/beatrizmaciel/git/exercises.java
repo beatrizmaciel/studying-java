e5ec18aec934dd59d008ac9071212597c6b1be2a
Beatriz C B Maciel <66186549+beatrizmaciel@users.noreply.github.com>
2021-05-01 20:19:06 -0300

    Update exercises.java

--- studying-java/src/br/com/beatrizmaciel/git/exercises.java
+++ studying-java/src/br/com/beatrizmaciel/git/exercises.java
@@ -66,6 +66,81 @@ public class exercises {
 			double areaQuadrado = lado * lado;
 			System.out.println("A área do quadrado é: " + areaQuadrado);
 			System.out.println("O dobro da área do quadrado é: " + areaQuadrado * 2);
+		
+			// Perguntar quanto ganha por hora e calcular o total por mês
+			
+			System.out.println("Quanto você ganha por hora?");
+			double hora = scan.nextDouble();
+			System.out.println("Quantos horas você trabalha por mês?");
+			double horasMes = scan.nextDouble();
+			System.out.println("O seu salário por mês é: " + hora * horasMes);
+			
+			// Transformar Farenheit em Celsius
+			System.out.println("Digite a temperatura em Fahrenheit:");
+			double farenheit = scan.nextDouble();
+			double celsius = (5 * (farenheit - 32) / 9);
+			System.out.println("A temperatura convertida para Celsius é: " + celsius);
+			
+			// Transformar Celsius em Farenheit
+			System.out.println("Digite a temperatura em Celsius:");
+			double celsius = scan.nextDouble();
+			double farenheit = celsius * 1.8 + 32;
+			System.out.println("A temperatura convertida para Farenheits é: " + farenheit);
+			
+			
+			// Pedir 2 números inteiros e 1 real. Calcular:
+			// 1. o produto do dobro do primeiro com a metade do segundo
+			// 2. a soma do triplo do primeiro com o terceiro
+			// 3. o terceiro elevado ao cubo
+		
+			System.out.println("Digite dois números inteiros e um número real:");
+			int numero1 = scan.nextInt();
+			int numero2 = scan.nextInt();
+			double numero3 = scan.nextDouble();
+			System.out.println("O produto do dobro do primeiro com a metade do segundo é: " + numero1 * 2 + numero2/2);
+			System.out.println("A soma do triplo do primeiro com o terceiro é: " + numero1 * 3 + numero3);
+			System.out.println("O terceiro elevado ao cubo é: " + numero3 * numero3);
+
+			// Peso ideal
+			System.out.println("Entre sua altura:");
+			double altura = scan.nextDouble();
+			double pesoIdeal = (72.7 * altura) - 58;
+			System.out.println("Seu peso ideal é: " + pesoIdeal);
+			
+			// Perguntar quanto ganha por hora, o número de horas
+			// trabalhadas por mês. Calcular o total do salário,
+			// sabendo que são descontados 11% para o Imposto de Renda
+			// 8% para o INSS e 5% para o sindicato. Indique:
+			// 1. O salário bruto
+			// 2. Quanto pagou ao INSS
+			// 3. O salário líquido
+			// 4. Calcule os descontos do salário líquido
+			
+			System.out.println("Quanto você ganha por hora?");
+			double hora = scan.nextDouble();
+			System.out.println("Quantas horas você trabalha por mês?");
+			double horasMes = scan.nextDouble();
+			double salarioBruto = hora * horasMes;
+			System.out.println("Seu salário bruto é: " + salarioBruto);
+			System.out.println("Você pagou " + ((salarioBruto*8)/100) + " ao INSS");
+			System.out.println("Você pagou " + ((salarioBruto*5)/100) + " ao sindicato");
+			System.out.println("Seu salário líquido é " + ((salarioBruto*76)/100));
+			System.out.println("O que foi descontado do seu salário foi: " + (salarioBruto*24)/100);
+			System.out.println("Em resumo. Seu salário bruto é " + salarioBruto + 
+					", foram descontados " + (salarioBruto*24)/100 + 
+					" e seu salário líquido é " + (salarioBruto*76)/100);
+			
+			
+			// Pedir tamanho de arquivo em MB + velocidade da internet em Mbps
+			// calcular e informar o tempo aproximado de download em minutos
+			
+			System.out.println("Qual é o tamanho (em MB) do arquivo para download?");
+			double tamanhoArquivo = scan.nextDouble();
+			System.out.println("Qual é a velocidade (em Mbps) da internet?");
+			double velocidadeInternet = scan.nextDouble();
+			double tempoDownload = tamanhoArquivo / velocidadeInternet;
+			System.out.println("O download do seu arquivo vai demorar " + tempoDownload + " minutos.");
+
 
 	}
 
